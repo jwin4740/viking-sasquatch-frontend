cd755f65ed87f8eafc9ddc75ef5ae1ac7b17d0c4
James Winkle <jjwinklejmj@gmail.com>
2022-05-01 23:34:03 -0500

    angular starter app rendering

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { HttpClient } from '@angular/common/http';
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA = {
+  name: 'Vegetables',
+
+  children: [
+    {
+      name: 'Green',
+      children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
+    },
+    {
+      name: 'Orange',
+      children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
+    },
+  ],
+};
+
+interface FactoryData {
+  id: number;
+  name: string;
+  lowerBoundChildNodes: number;
+  upperBoundChildNodes: number;
+  numberChildrenToCreate: number;
+  createdDate: string;
+  updatedDate: string;
+  chidren: Array<any>;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'angular-tree-project';
+
+  mockData: any = TREE_DATA;
+
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    // this.httpClient.get('http://localhost:3000/api/factory').subscribe(data => {
+    //   console.log(data)
+    //   this.httpClient.get('http://localhost:3000/api/child').subscribe(data => {
+    //     console.log(data)
+    //   // this.dataSource.data = data
+    // })
+  }
+
+  editParent(): void {}
+
+  hasChild = (_: number, node: FoodNode) =>
+    !!node.children && node.children.length > 0;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTreeModule,
+    MatIconModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
