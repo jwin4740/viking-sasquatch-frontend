ce2f645dd3fc1a6aff69c685af00639b6869461f
James Winkle <jjwinklejmj@gmail.com>
2022-05-05 07:05:39 -0400

    add back prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
-// export const environment = {
-//   production: true,
-//   baseUrl: 'https://viking-sas-backend.azurewebsites.net',
-// };
-
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://viking-sas-backend.azurewebsites.net',
 };
